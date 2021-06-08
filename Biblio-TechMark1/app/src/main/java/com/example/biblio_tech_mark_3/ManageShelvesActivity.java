3485a8003e4d51408f8498655e4300b5777ef6ab
knight-captain <44849526+knight-captain@users.noreply.github.com>
2021-06-08 15:57:05 -0400

    Mark 3
    using MAr1's directory for Mark 3

---
+++ Biblio-TechMark1/app/src/main/java/com/example/biblio_tech_mark_3/ManageShelvesActivity.java
@@ -0,0 +1,14 @@
+package com.example.biblio_tech_mark_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageShelvesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_shelves);
+    }
+}
