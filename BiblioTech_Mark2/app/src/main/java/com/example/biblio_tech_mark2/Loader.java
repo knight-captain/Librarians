8946be8ad6ec7c12a010063411a6a810427ded1b
knight-captain <44849526+knight-captain@users.noreply.github.com>
2021-06-08 15:11:31 -0400

    This is a disaster!
    Mark 2 has gotten too incomprehensible, especially with the "basic" activity I picked. I'm making a simple Mark 3, using principles we've used in class...

--- BiblioTech_Mark2/app/src/main/java/com/example/biblio_tech_mark2/Loader.java
+++ BiblioTech_Mark2/app/src/main/java/com/example/biblio_tech_mark2/Loader.java
@@ -1,6 +1,5 @@
 package com.example.biblio_tech_mark2;
 
-import android.app.Activity;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,24 +8,26 @@ import java.io.FileNotFoundException;
 
 public class Loader extends AppCompatActivity implements Runnable{
 
-    private Activity ma;
+//    private Activity ma;
     public static final String TAG = "!!!Loader!!!";
 
     @Override
     public void run() {
         Shelf library = new Shelf("test");
 
-
+        // Gets temp library from LibraryLoadFromJson.
+        // TODO should get internal data instead.
         try {
             Log.i(TAG,"3");
             Shelf readFile = LibraryLoadFromJson.readFile();
-            Log.i(TAG,"4");
+
             Log.i(TAG, String.valueOf(readFile));
+
             for(Book b : readFile.getBooks()){
                 System.out.println(b.getTitle());
             }
 
-//            Intent intent = new Intent(ma, SecondFragment.class);
+//            Intent intent = new Intent(this, MainActivity.class);
 //            intent.putExtra(TAG, String.valueOf(readFile));
 //            startActivity(intent);
 
@@ -36,7 +37,7 @@ public class Loader extends AppCompatActivity implements Runnable{
         }
     }
 
-    public Loader (MainActivity ma){
-        this.ma = new Activity();
-    } //may need WeakReference
+//    public Loader (MainActivity ma){
+//        this.ma = new Activity();
+//    } //may need WeakReference
 }
--- BiblioTech_Mark2/app/src/main/java/com/example/biblio_tech_mark2/MainActivity.java
+++ BiblioTech_Mark2/app/src/main/java/com/example/biblio_tech_mark2/MainActivity.java
@@ -2,82 +2,23 @@ package com.example.biblio_tech_mark2;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.biblio_tech_mark2.databinding.ActivityMainBinding;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
     public static final String TAG = "!!!Main activity!!!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        Log.i(TAG,"Loading test library");
-//        QuickSave.writeFileOnInternalStorage(this.getApplicationContext()); //TODO this is causing problems...
+        setContentView(R.layout.activity_main);
 
         Log.i(TAG,"Starting loader and Thread");
-        Loader loader = new Loader(this);
+        Loader loader = new Loader(/*this*/);
         Thread loading = new Thread((Runnable) loader, "loader");
         loading.start();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //This is where "add book" would go! -mcm
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
