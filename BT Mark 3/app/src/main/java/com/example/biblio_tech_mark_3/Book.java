11e600aa2a2a490ad58e7fcf58c86b6710d0f66a
knight-captain <44849526+knight-captain@users.noreply.github.com>
2021-07-07 17:25:19 -0400

    trying again

---
+++ BT Mark 3/app/src/main/java/com/example/biblio_tech_mark_3/Book.java
@@ -0,0 +1,29 @@
+package com.example.biblio_tech_mark_3;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Book {
+    @SerializedName("TITLE")
+    private String title;
+    @SerializedName("Author")
+    private String author;
+    @SerializedName("Series")
+    private String series;
+//    private int pages;
+
+    public String getAuthor() { return author; }
+
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getTitle() { return title; }
+
+    //add this book to this/these shelves
+
+    public void setTitle(String title) { this.title = title; }
+
+    public Book (){
+        this.title = "unknown book";
+        this.author = "unknown author";
+        this.series = "none";
+    }
+}
--- Project_7_2/Librarians-master/BT Mark 3/app/src/main/java/com/example/biblio_tech_mark_3/APIHelper.java
+++
@@ -1,4 +0,0 @@
-package com.example.biblio_tech_mark_3;
-
-public class APIHelper {
-}
